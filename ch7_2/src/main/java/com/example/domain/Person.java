d79845bb8e9b4878ac6624645ca0884a2be07d7a
qwang1990 <wangqi2179@163.com>
2016-07-07 21:19:01 +0800

    f

---
+++ ch7_2/src/main/java/com/example/domain/Person.java
@@ -0,0 +1,33 @@
+package com.example.domain;
+
+/**
+ * Created by wangqi on 16/7/7.
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
